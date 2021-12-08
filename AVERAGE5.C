8ac5b5f34e83bb8415868dd26bd3fb6a12f8ab2d
Mayurimeshram <95417199+Mayurimeshram@users.noreply.github.com>
2021-12-08 14:21:10 +0530

    Add files via upload

---
+++ AVERAGE5.C
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n1,n2,n3,n4,n5,total=0,c=0;
+float average;
+printf("enter the fisrt number");
+scanf("%d",&n1);
+printf("enter the second number");
+scanf("%d",&n2);
+printf("enter third number");
+scanf("%d",&n3);
+printf("enter fouth number");
+scanf("%d",&n4);
+printf("enter the fiveth number");
+scanf("%d",&n5);
+if(n1>=5)
+{
+c++;
+total=total+i;
+}
+if(n2>=5)
+{
+c++;
+total=total+i;
+}
+if(n3>=5)
+{
+c++;
+total=total+i;
+}
+if(n4>=5)
+{
+c++;
+total=total+i;
+}
+if(n5>=5)
+{
+c++;
+total=total+i;
+}
+average=total/c;
+{
+printf("\n enter the count number is positive",c);
+printf("\n enter the average of all positive number %2f\n",average);
+}
+getch();
+}
